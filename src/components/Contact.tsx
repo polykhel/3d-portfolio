8c339551b9a93628b3916d3be90b1e5d39af8381
Michael Pollente <pollentem@gmail.com>
2023-09-21 22:58:35 +0800

    Add Contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,123 @@
+import emailjs from '@emailjs/browser';
+import { motion } from 'framer-motion';
+import { BaseSyntheticEvent, FormEvent, useRef, useState } from 'react';
+import { SectionWrapper } from '../hoc';
+import { styles } from '../styles.ts';
+import { slideIn } from '../utils/motion.ts';
+import { EarthCanvas } from './canvas';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef<HTMLFormElement>(null);
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: BaseSyntheticEvent) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs
+      .send(
+        'service_waoajsr',
+        'template_6cvjmes',
+        {
+          from_name: form.name,
+          to_name: 'Michael Pollente',
+          from_email: form.email,
+          to_email: 'pollentem@gmail.com',
+          message: form.message,
+        },
+        '32aQ43O5Njp9cUtlm'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert('Thank you. I will get back to you as soon as possible.');
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert('Something went wrong');
+        }
+      );
+  };
+
+  return (
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
+      <motion.div
+        variants={slideIn('left', 'tween', 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-12 flex flex-col gap-8'
+        >
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Name</span>
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder='What do you want to say?'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+            />
+          </label>
+
+          <button
+            type='submit'
+            className='bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+      >
+        <EarthCanvas />
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, 'contact');
--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -1,5 +1,34 @@
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../Loader.tsx';
+
 const Earth = () => {
-  return <div>Earth</div>;
+  const earth = useGLTF('./planet/scene.gltf');
+
+  return (
+    <primitive object={earth.scene} scale={2.5} position-y={0} position-x={0} />
+  );
 };
 
-export default Earth;
+const EarthCanvas = () => (
+  <Canvas
+    shadows={true}
+    frameloop='demand'
+    gl={{ preserveDrawingBuffer: true }}
+    camera={{ fov: 45, near: 0.1, far: 200, position: [-4, 3, 6] }}
+  >
+    <Suspense fallback={<Loader />}>
+      <OrbitControls
+        autoRotate={true}
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+
+      <Earth />
+    </Suspense>
+  </Canvas>
+);
+
+export default EarthCanvas;
