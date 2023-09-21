c612c14d5aaa48b4a472735f04e905a4308577d2
Michael Pollente <pollentem@gmail.com>
2023-09-21 20:54:56 +0800

    Add Tech section

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -1,5 +1,45 @@
-const Ball = () => {
-  return <div>Ball</div>;
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import Loader from '../Loader.tsx';
+
+const Ball = ({ imgUrl }: { imgUrl: string }) => {
+  const [decal] = useTexture([imgUrl]);
+
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={1}></ambientLight>
+      <directionalLight position={[0, 0, 0, 0.05]}></directionalLight>
+      <mesh castShadow={true} receiveShadow={true} scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          hash='#fff8eb'
+          polygonOffset={true}
+          polygonOffsetFactor={-5}
+          flatShading={true}
+        />
+        <Decal
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          map={decal}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
 };
 
-export default Ball;
+const BallCanvas = ({ icon }: { icon: string }) => {
+  return (
+    <Canvas gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<Loader />}>
+        <OrbitControls enableZoom={false} autoRotate={true} autoRotateSpeed={1.5} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
